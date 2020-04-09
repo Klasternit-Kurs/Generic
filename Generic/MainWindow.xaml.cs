df0ee4f183a877337f5c956354f8cfaabd4bc74b
Dragan Pepic <d.pepic@hotmail.com>
2020-04-09 19:35:39 +0200

    Add project files.

---
+++ Generic/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Generic
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			MojaKlasa<int, object, string> asd = new MojaKlasa<int, object, string>();
+			asd.bla = 6;
+			asd.ProveriNull(5);
+
+
+			MojaKlasa<string, object, string> dsa = new MojaKlasa<string, object, string>();
+			dsa.bla = "asd";
+		}
+	}
+
+	public class MojaKlasa<T, U, zklj>
+	{
+		List<T> nekaLista;
+		public T bla;
+		public U asd;
+		public zklj treci;
+
+		public bool ProveriNull(T nesto)
+		{
+			switch(nesto)
+			{
+				case int i:
+					if (i == 0)
+						return true;
+					break;
+				case string s:
+					if (s == null)
+						return true;
+					break;
+			}
+			return false;
+		}
+
+	}
+}
